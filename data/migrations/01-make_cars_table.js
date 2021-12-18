bd07ff6beb1b6a377ceb19953f1c3fda26a79e13
joey-bertschler <joey.bertschler@gmail.com>
2021-12-18 18:53:58 +0400

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,16 @@
 exports.up = function (knex) {
-  // DO YOUR MAGIC
+  return knex.schema.createTable('cars', table => {
+    table.increments('id') //no need to write id because id is the default
+    table.string('vin', 17).unique().notNullable();
+    // .text is for long text, .string is for short text, think s for short
+    table.string('make', 128).notNullable();
+    table.string('model', 128).notNullable();
+    table.integer('mileage').unsigned().notNullable(); //unsigned is for positive numbers only
+    table.string('title', 128).defaultTo('clean');
+    table.string('transmission', 128).defaultTo('automatic');
+  });
 };
 
 exports.down = function (knex) {
-  // DO YOUR MAGIC
-};
+  return knex.schema.dropTable('cars');
+}//always create a down function to rollback changes in other words to undo the changes
