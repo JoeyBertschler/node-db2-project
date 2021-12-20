a9d713b4300309a265c9dc23fc3b20bb70c0dede
joey-bertschler <joey.bertschler@gmail.com>
2021-12-20 10:27:44 +0400

    wip

--- api/server.js
+++ api/server.js
@@ -5,9 +5,6 @@ const server = express()
 server.use(express.json())
 
 server.use('*', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
     next({ status: 404, message: 'not found'})
 })
 
@@ -17,6 +14,6 @@ server.use((err, req, res, next) => { //eslint-disable-line
     })
 })
 
-server.listen(`listening on Port ${PORT}`)
 
 module.exports = server
+
