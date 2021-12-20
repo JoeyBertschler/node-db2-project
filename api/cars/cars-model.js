f6028f1b5eae2837b841f66781a5a647a3b51bcd
joey-bertschler <joey.bertschler@gmail.com>
2021-12-20 10:48:05 +0400

    wip

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,5 +1,7 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
-  
+  return db('cars')
 }
 
 const getById = () => {
@@ -8,4 +10,10 @@ const getById = () => {
 
 const create = () => {
 
+}
+
+module.exports = { //module.exports is a variable that can be used to export a function or variable
+    getAll,
+    getById,
+    create
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,8 +1,15 @@
 const express = require("express")
+const Car = require("./cars-model")
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-    res.json('getting all cars')
+   // res.json('getting all cars')
+   try {
+        const cars = await Car.getAll()
+        res.status(200).json(cars)
+    } catch (err) {
+        next(err)
+    }
 })
 router.get('/:id', async (req, res, next) => {
     res.json(`getting car with id ${req.params.id}`)
